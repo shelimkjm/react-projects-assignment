e64e0949b9c4a26e622588cd7173fae4a5c75e0d
shelim <shelimkjm@gmail.com>
2024-03-21 18:04:37 +0600

    recheck again

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,32 +1,17 @@
-// import banner from './assets/Frame.png'
-// const Banner = () => {
-//     return (
-//         <div className="container mx-auto mt-8 space-y-5 bg-[url('banner')">
-// <h1 className="text-center text-4xl font-bold">Discover an exceptional cooking <br />class tailored for you!</h1>
 
-// <p className="text-center">Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ <br /> coding problems to become an exceptionally well world-class Programmer.</p>
-
-
-// <div className="flex gap-4 justify-center">
-//     <button className="bg-[#0BE58A] p-3  rounded-full font-bold ">Explore Now</button>
-//     <button className="border border-black rounded-full p-3 font-bold">Our Feedback</button>
-// </div>
-            
-//         </div>
-//     );
-// };
-
-// export default Banner;
-import banner from '../src/assets/Groupp.png';
+import banner from '../src/assets/bg.png';
 
 const Banner = () => {
     return (
-        <div className="container mx-auto mt-8 lg:space-y-36 bg-cover bg-no-repeat" style={{ backgroundImage: `url(${banner})` }}>
-            <p>k  </p>
-            <p>Master B</p>
-           <p>p</p>
-            <p> v</p>
-            <p>d</p>
+        <div className="container mx-auto mt-8  flex flex-col justify-center items-center space-y-12 rounded-md  bg-cover bg-no-repeat min-h-screen" style={{ backgroundImage: `url(${banner})` }}>
+       <div className='space-y-10'>  <h1 className='text-white font-bold text-4xl text-center'> Discover an exceptional cooking <br />
+class tailored for you!</h1>
+<p className='text-2xl text-white text-center'>Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding <br />
+problems to become an exceptionally well world-class Programmer</p></div>
+
+
+<div className='flex justify-center items-center gap-4 '><button className='text-white font-normal bg-green-600 p-2 rounded-md py-2 '>Explore Now</button>
+<button className='text-white font-normal  shadow-lg border-white'>Our Feedback</button></div>
 
         </div>
     );
